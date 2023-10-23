2f5dd6701bf0ac45dfbfa5f62383ac397ea135e4
AlexxBelous <connectionwithme1@gmail.com>
2023-10-23 06:05:06 +0300

    add new part code

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -9,6 +9,16 @@ class BlogController extends Controller
     public function indexAction()
     {
         $db = DB::getInstance();
+        $sql = "INSERT INTO articles (`title`, `body`) VALUES  (:title, :body)";
+        $bind = ['title' => 'new article', 'body' => 'article body'];
+        $query = $db->execute($sql, $bind);
+        $lastId = $query->lastInsertId();
+        H::dnd($lastId);
+//        $sql = "SELECT * FROM articles";
+//        $query = $db->query($sql);
+//        $articles = $query->results();
+//        $count = $query->lastInsertId();
+//        H::dnd($count);
         $this->view->setSiteTitle('Newest Article');
         $this->view->render();
     }
--- core/DB.php
+++ core/DB.php
@@ -8,7 +8,8 @@ use Core\{Config, H};
 
 class DB
 {
-    protected  $_dbh, $_result, $_lastInsertId, $_rowCount = 0, $_featchType = PDO::FETCH_OBJ, $_class, $_error = false;
+    protected $_dbh, $_result, $_lastInsertId, $_rowCount = 0, $_featchType = PDO::FETCH_OBJ, $_class, $_error = false;
+    protected $_stmt;
     protected static $_db;
 
     public function __construct()
@@ -32,9 +33,48 @@ class DB
 
     public static function getInstance()
     {
-        if(!self::$_db){
+        if (!self::$_db) {
             self::$_db = new self();
         }
         return self::$_db;
+    }
+
+    public function execute($sql, $bind = [])
+    {
+        $this->_result = null;
+        $this->_lastInsertId = null;
+        $this->_error = false;
+        $this->_stmt = $this->_dbh->prepare($sql);
+        if (!$this->_stmt->execute($bind)) {
+            $this->_error = true;
+        } else {
+            $this->_lastInsertId = $this->_dbh->lastInsertId();
+        }
+        return $this;
+    }
+
+    public function query($sql, $bind = [])
+    {
+        $this->execute($sql, $bind);
+        if (!$this->_error) {
+            $this->_rowCount = $this->_stmt->rowCount();
+            $this->_result = $this->_stmt->fetchAll($this->_featchType);
+        }
+        return $this;
+    }
+
+    public function results()
+    {
+        return $this->_result;
+    }
+
+    public function count()
+    {
+        return $this->_rowCount;
+    }
+
+    public function lastInsertId()
+    {
+        return $this->_lastInsertId;
     }
 }
